6927e0fb631a74003115f802ed45cca8c43554d2
ankidets <hugo.kirch@epitech.eu>
2022-07-11 14:02:46 +0200

    [TEST] correcting dump error

--- tests/generateWorkflow.sh
+++ tests/generateWorkflow.sh
@@ -5,20 +5,22 @@ cat buildtest > output
 cat mandatorytest >> output
 #generate job for mandatory tests
 for d in mandatory/*; do
-    if NAME=$(ls "$d" | grep ".*\.avm") ; then
+    if FILE=$(ls "$d" | grep ".*\.avm") ; then
         TEMPLATE=$(cat tasktemplate)
-        NAME=${NAME%.avm}
+        NAME=${FILE%.avm}
         echo "Generating task templated test for $d, name test: $NAME"
         TEMPLATE=${TEMPLATE//|FOLDER|/$d}
         TEMPLATE=${TEMPLATE//|NAME|/$NAME}
+        TEMPLATE=${TEMPLATE//|FILE|/$FILE}
         echo "$TEMPLATE" >> output
-    elif NAME=$(ls "$d" | grep ".*\.input") ; then
-        NAME=${NAME%.input}
+    elif FILE=$(ls "$d" | grep ".*\.input") ; then
+        NAME=${FILE%.input}
         echo "Generating input templated test for $d, name test: $NAME"
         TEMPLATE=$(cat inputtemplate)
         NAME=${NAME%.input}
         TEMPLATE=${TEMPLATE//|FOLDER|/$d}
         TEMPLATE=${TEMPLATE//|NAME|/$NAME}
+        TEMPLATE=${TEMPLATE//|FILE|/$FILE}
         echo "$TEMPLATE" >> output
     fi
 
