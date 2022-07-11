c688eca17c7100aa0737a27325eb06a69c0f396e
ankidets <hugo.kirch@epitech.eu>
2022-07-11 14:09:01 +0200

    [TEST] adding extra tests and moving templates to a folder

--- tests/generateWorkflow.sh
+++ tests/generateWorkflow.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-cat buildtest > output
-cat mandatorytest >> output
+cat generators_templates/buildtest > output
+cat generators_templates/mandatorytest >> output
 #generate job for mandatory tests
 for d in mandatory/*; do
     if FILE=$(ls "$d" | grep ".*\.avm") ; then
-        TEMPLATE=$(cat tasktemplate)
+        TEMPLATE=$(cat generators_templates/tasktemplate)
         NAME=${FILE%.avm}
         echo "Generating task templated test for $d, name test: $NAME"
         TEMPLATE=${TEMPLATE//|FOLDER|/$d}
@@ -16,7 +16,29 @@ for d in mandatory/*; do
     elif FILE=$(ls "$d" | grep ".*\.input") ; then
         NAME=${FILE%.input}
         echo "Generating input templated test for $d, name test: $NAME"
-        TEMPLATE=$(cat inputtemplate)
+        TEMPLATE=$(cat generators_templates/inputtemplate)
+        NAME=${NAME%.input}
+        TEMPLATE=${TEMPLATE//|FOLDER|/$d}
+        TEMPLATE=${TEMPLATE//|NAME|/$NAME}
+        TEMPLATE=${TEMPLATE//|FILE|/$FILE}
+        echo "$TEMPLATE" >> output
+    fi
+
+done
+cat generators_templates/extratest >> output
+for d in extra/*; do
+    if FILE=$(ls "$d" | grep ".*\.avm") ; then
+        TEMPLATE=$(cat generators_templates/tasktemplate)
+        NAME=${FILE%.avm}
+        echo "Generating task templated test for $d, name test: $NAME"
+        TEMPLATE=${TEMPLATE//|FOLDER|/$d}
+        TEMPLATE=${TEMPLATE//|NAME|/$NAME}
+        TEMPLATE=${TEMPLATE//|FILE|/$FILE}
+        echo "$TEMPLATE" >> output
+    elif FILE=$(ls "$d" | grep ".*\.input") ; then
+        NAME=${FILE%.input}
+        echo "Generating input templated test for $d, name test: $NAME"
+        TEMPLATE=$(cat generators_templates/inputtemplate)
         NAME=${NAME%.input}
         TEMPLATE=${TEMPLATE//|FOLDER|/$d}
         TEMPLATE=${TEMPLATE//|NAME|/$NAME}
@@ -26,7 +48,7 @@ for d in mandatory/*; do
 
 done
 
-cat mirroringmodule >> output
+cat generators_templates/mirroringmodule >> output
 
 cp output ../.github/workflows/c-cpp.yml
 rm output
