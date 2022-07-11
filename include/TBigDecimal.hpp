2a8a866343cb155f4df88d39e982fe31f85985c8
fabrac <fabrice.ilic-erb@epitech.eu>
2022-07-11 18:03:02 +0200

    Bit decimal sub

--- include/TBigDecimal.hpp
+++ include/TBigDecimal.hpp
@@ -22,11 +22,15 @@ namespace avm{
             eOperandType getType() const;
             void setValue(std::string v);
             std::shared_ptr<TBigDecimal> operator+(const std::shared_ptr<TBigDecimal> rhs);
+            std::shared_ptr<TBigDecimal> operator-(const std::shared_ptr<TBigDecimal> rhs);
+            std::shared_ptr<TBigDecimal> operator*(const std::shared_ptr<TBigDecimal> rhs);
 
         private:
             bool negative;
             std::string integer;
             std::string decimal;
             std::shared_ptr<avm::TBigDecimal> add(const std::shared_ptr<avm::TBigDecimal> rhs);
+            std::shared_ptr<avm::TBigDecimal> sub(const std::shared_ptr<avm::TBigDecimal> rhs);
+            std::shared_ptr<avm::TBigDecimal> mul(const std::shared_ptr<avm::TBigDecimal> rhs);
     };
 }
--- src/Operand.cpp
+++ src/Operand.cpp
@@ -24,8 +24,6 @@ std::shared_ptr<avm::IOperand> avm::Operand::operator+(const std::shared_ptr<avm
     if (type == avm::BIGDECIMAL) {
         auto a = std::make_shared<avm::TBigDecimal>(this->toString());
         auto b = std::make_shared<avm::TBigDecimal>(rhs->toString());
-        std::cout << "a " << a->toString() << std::endl;
-        std::cout << "b " << b->toString() << std::endl;
         return (*a + b);
     }
 
@@ -36,6 +34,11 @@ std::shared_ptr<avm::IOperand> avm::Operand::operator+(const std::shared_ptr<avm
 std::shared_ptr<avm::IOperand> avm::Operand::operator-(const std::shared_ptr<avm::IOperand> rhs) const
 {
     avm::eOperandType type = (this->getType() > rhs->getType()) ? this->getType() : rhs->getType();
+    if (type == avm::BIGDECIMAL) {
+        auto a = std::make_shared<avm::TBigDecimal>(this->toString());
+        auto b = std::make_shared<avm::TBigDecimal>(rhs->toString());
+        return (*a - b);
+    }
 
     std::string value = avm::Factory::convert(type, std::to_string(std::stold(this->toString()) - std::stold(rhs->toString())));
 
@@ -45,7 +48,11 @@ std::shared_ptr<avm::IOperand> avm::Operand::operator-(const std::shared_ptr<avm
 std::shared_ptr<avm::IOperand> avm::Operand::operator*(const std::shared_ptr<avm::IOperand> rhs) const
 {
     avm::eOperandType type = (this->getType() > rhs->getType()) ? this->getType() : rhs->getType();
-
+    if (type == avm::BIGDECIMAL) {
+        auto a = std::make_shared<avm::TBigDecimal>(this->toString());
+        auto b = std::make_shared<avm::TBigDecimal>(rhs->toString());
+        return (*a * b);
+    }
     std::string value = avm::Factory::convert(type, std::to_string(std::stold(this->toString()) * std::stold(rhs->toString())));
 
     return (avm::Factory::createOperand(type, value));
@@ -59,6 +66,7 @@ std::shared_ptr<avm::IOperand> avm::Operand::operator/(const std::shared_ptr<avm
     long double result = std::stold(this->toString()) / std::stold(rhs->toString());
     std::string v = avm::Utils::setPrecision(result, 20);
     std::string value = avm::Factory::convert(type, v);
+
 
     return (avm::Factory::createOperand(type, value));
 }
